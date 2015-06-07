ae5c39bf5f938d207887492773a732fa73b8c932
擢秀 <quanruzhuoxiu@gmail.com>
2015-06-07 11:31:04 +0800

    添加右侧实时显示截图区域
    保存功能由于Chrome同源策略，无法使用，Firefox下正常。

--- screenShot.js
+++ screenShot.js
@@ -3,10 +3,12 @@ window.onload = drag;
 function drag() {
     var rect = document.getElementById("shotRect"), // 拖曳框
         loadBtn = document.getElementById("loadBtn"), //载入图片按钮
+        saveBtn = document.getElementById("saveBtn"), //保存图片按钮
         originImg = document.getElementById("originImg"),
         brConer = document.getElementsByClassName("resizeBR")[0]; //右下角调节大小
     rect.addEventListener("mousedown", dragDown); //按住截图框
     loadBtn.addEventListener("click", loadImg); //载入图片
+    saveBtn.addEventListener("click", saveImg); //保存图片
     originImg.addEventListener("mousedown", longClick); //长按弹出截图框
     originImg.addEventListener("mouseup", clearTime); //释放清楚定时器
     brConer.addEventListener("mousedown", resizeDown); //按住调节大小
@@ -41,6 +43,7 @@ function dragDown(event) {
         }
         shotRect.style.left = disL + 'px'; //重新计算截图框的相对位置
         shotRect.style.top = disT + 'px';
+        updateRect(disL, disT, shotRect.offsetWidth, shotRect.offsetHeight);
     }
 
     function mouseUp(event) {
@@ -49,8 +52,14 @@ function dragDown(event) {
     }
 }
 
-function loadImg(event) {//载入图片
+function loadImg(event) { //载入图片
     document.getElementById("originImg").setAttribute('src', 'vegetable.jpg');
+}
+
+function saveImg(event) {
+    var image = c.toDataURL("image/png");
+    var w = window.open('about:blank', 'image from canvas');
+    w.document.write("<img src='" + image + "' alt='from canvas'/>");
 }
 
 function longClick(event) { //长按弹出截图框
@@ -62,7 +71,8 @@ function longClick(event) { //长按弹出截图框
             disY = event.clientY - shotRect.offsetHeight + 10;
         shotRect.style.left = disX + 'px';
         shotRect.style.top = disY + 'px';
-        updateRect(shotRect.style.top, shotRect.style.top + shotRect.offsetHeight, shotRect.style.left, shotRect.style.left + shotRect.offsetWidth);
+        initCanvas();
+        updateRect(disX, disY, shotRect.offsetWidth, shotRect.offsetHeight);
     }, 1000);
 }
 
@@ -94,6 +104,7 @@ function resizeDown(event) {
         //计算移动后的Rect新大小
         shotRect.style.width = finalX - x + 'px';
         shotRect.style.height = finalY - y + 'px';
+        updateRect(x - 15, y - 15, shotRect.offsetWidth, shotRect.offsetHeight);
     }
     //停止事件 
     function mouseUp() {
@@ -102,6 +113,20 @@ function resizeDown(event) {
         document.removeEventListener("mouseup", mouseUp);
     }
 }
-function updateRect (t,b,l,r) {
-	
+
+function initCanvas() {//初始化画布
+    c = document.getElementById("showPre");
+    ctx = c.getContext("2d");
+    img = document.getElementById("originImg");
+}
+
+function updateRect(x, y, w, h) {//更新画布
+    ctx.clearRect(0, 0, 450, 450); //清空画布
+    ctx.save(); //保存了当前ctx的状态
+    ctx.beginPath(); //开始画路径
+    ctx.rect(x, y, w, h); //裁剪区域
+    ctx.closePath(); //清空路径，不然每次都会保留上次的路径
+    ctx.clip(); //裁剪
+    ctx.drawImage(img, 0, 0, 450, 450); //绘图
+    ctx.restore(); //恢复到刚刚保存的状态
 }
